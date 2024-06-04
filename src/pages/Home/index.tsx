693fa90c4b34bf424057b50c3e1b7f3a6400e716
Icaro <icarovscarvalho@gmail.com>
2024-06-04 16:33:25 -0300

    Estilo da Pagina Home em construção

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,37 @@
-
+import styles from "./styles.module.css"
+import openBook from "../../assets/images/openBook.svg"
+import {ColletionBook} from "../../components/ColletionBook";
 
 export function Home(){
     return(
         <>
-            Home
+            <main className={styles.main}>
+                <div className={styles.welcome}>
+                    <h1>Bem Vindo(a)</h1>
+                    <p>Que diferentes mundos você quer explorar hoje?</p>
+                    <img src={openBook} alt="Imagem de livro aberto"/>
+                    <button>Quer uma sugestão?</button>
+                </div>
+
+                <div className={styles.bookSearchField}>
+                    <div className={styles.titleBar}>
+                        <h2>Em Alta</h2>
+                        <button>Veja mais</button>
+                    </div>
+                    <div className={styles.books}>
+                        <ColletionBook/>
+                        <ColletionBook/>
+                        <ColletionBook/>
+                        <ColletionBook/>
+                        <ColletionBook/>
+                        <ColletionBook/>
+                        <ColletionBook/>
+                        <ColletionBook/>
+                        <ColletionBook/>
+                        <ColletionBook/>
+                    </div>
+                </div>
+            </main>
         </>
     )
 }
