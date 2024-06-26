8dfecd25c1f2fc0cdb1cfddcb17c3867f451a50b
Icaro <icarovscarvalho@gmail.com>
2024-06-25 21:48:41 -0300

    Function updatWindowWidth - Manutenção da função de verificação de recize

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -6,10 +6,40 @@ import { MdArrowOutward } from "react-icons/md";
 import {
   BookType
 } from "../../@types/appTypes.ts";
+import {
+  useEffect
+} from "react";
+import {
+  signal
+} from "@preact/signals-react";
 
 // const imgTest = "https://books.google.com/books/publisher/content/images/frontcover/VLg-EAAAQBAJ?fife=w240-h345"
 
 export function Details({cover, title, authors, description}:BookType) {
+  let bookScale:number = 1;
+  const bookStyle = signal({
+    transform: `rotateX(10deg) rotateY(-20deg) scale(${bookScale})`
+  })
+
+  function updateWindowWidth() {
+    const gapValue = Math.floor(window.innerWidth - 640);
+    const gap = gapValue > 0 ? gapValue < 560 ? gapValue : 560 : 0
+    const bookScaleCalc = Math.floor(gap / 28) / 100;
+    bookScale = 0.8 + bookScaleCalc;
+    bookStyle.value = {
+      transform: `rotateX(10deg) rotateY(-20deg) scale(${bookScale})`
+    }
+
+    console.log(bookScale);
+  }
+
+  useEffect(() => {
+    updateWindowWidth()
+    window.addEventListener('resize', updateWindowWidth)
+
+    return () => window.removeEventListener('resize', updateWindowWidth)
+  }, []);
+
   return(
     <>
       <div className={styles.container}>
@@ -21,9 +51,8 @@ export function Details({cover, title, authors, description}:BookType) {
           </nav>
           {/*<div className={styles.coverImg} style={{backgroundImage: `url(${cover})`}} />*/}
           <div
-            className={styles.scene}>
-            <div
-              className={styles.book}>
+            className={styles.scene} >
+            <div className={styles.book} style={bookStyle.value}>
               <div
                 className={`${styles.face} ${styles.front} `} style={{backgroundImage: `url(${cover})`}}></div>
               <div
